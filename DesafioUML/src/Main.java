1e319ae001aecf1fc89deebdc0e975a4eeaf1b23
Fabiano <fabianoquirinos@outlook.com>
2024-06-14 11:00:03 -0300

    Commit do Código

---
+++ DesafioUML/src/Main.java
@@ -0,0 +1,8 @@
+import iphone.Iphone;
+
+public class Main {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+
+    }
+}
---
+++ DesafioUML/src/dispositivos/interfaces/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package dispositivos.interfaces;
+
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
